c22f1f5e87f50b5737fe825500c71e5cd370363d
Paul Bunker <paulBunker@khws.co.uk>
2017-04-04 14:11:33 +0100

    Move polyfill

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
+// Polyfill for custom events used in card.js;
+import customEvents from '../polyfills/customEvent';
+customEvents();
+
 import Grid from './modules/grid';
 
 new Grid(document.querySelector('.grid'));
