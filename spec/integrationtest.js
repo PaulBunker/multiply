23be36dbc5240fe0ff0becb0a11a4d66907d29dd
Paul Bunker <paulmarkbunker@gmail.com>
2017-04-09 13:23:31 +0100

    revert tests to a reasonable state

--- spec/integrationtest.js
+++ spec/integrationtest.js
@@ -30,6 +30,16 @@ describe('Multiply test:', function() {
 
   });
 
+
+
+
+  // Test to ensure tests work
+  it('Test can get card', done => {
+    this.driver.findElement(selenium.By.tagName('body'));
+    expect(true).toBe(true);
+    done();
+  });
+
   it('Should exist', done => {
     var driver = this.driver;
     var card = driver.wait(selenium.until.elementLocated(selenium.By.css('.card')));
@@ -39,71 +49,44 @@ describe('Multiply test:', function() {
   });
 
 
-  // Test to ensure tests work
+  // Test to ensure we highlight the
   it('Should highlight', done => {
-    var hi = this.driver.findElement(selenium.By.tagName('body'));
-    expect(true).toBe(true);
+    var element = this.driver.findElement(selenium.By.css('.card'));
+    element.click();
+    element.getAttribute('class').then(className => {
+      expect(className).toBe('card card__highlight');
+    });
     done();
   });
 
-  // // Test to ensure tests work
-  // it('Should highlight', done => {
-  //   var driver = this.driver;
-  //   setTimeout(function(){
-  //     var hi = driver.findElement(selenium.By.css('.card'));
-  //     expect(true).toBe(true);
-  //     done();
-  //   }, 5000);
-  // });
+  // Test to ensure we have the right number of cards
+  it('Should be 144 cards', done => {
+    this.driver.findElements(selenium.By.className('card')).then(elements => {
+      expect(elements.length).toBe(144);
+    });
+    done();
+  });
 
+  // Test to ensure all cards are highlighted after clicking 1'st card
+  it('Should highlight all 144 cards', done => {
+    var element = this.driver.findElement(selenium.By.css('.card'));
+    element.click();
+    this.driver.findElements(selenium.By.className('card__highlight')).then(elements => {
+      expect(elements.length).toBe(144);
+    });
+    done();
+  });
 
-
-  // Test to ensure we highlight the
-  // it('Should highlight', done => {
-    // setTimeout(function(){
-    //   var element = this.driver.findElement(selenium.By.css('.card'));
-    //   element.click();
-    //   element.getAttribute('class').then(className => {
-    //     expect(className).toBe('card card__highlight');
-    //   });
-    //   done();
-    // }, 5000);
-  //});
-
-  // // Test to ensure we have the right number of cards
-  // it('Should be 144 cards', done => {
-  //   setTimeout(function(){ var hi = this.driver.findElement(selenium.By.css('.card')); }, 300000);
-
-  //   this.driver.findElements(selenium.By.className('card')).then(elements => {
-  //     expect(elements.length).toBe(144);
-  //   });
-  //   done();
-  // });
-
-  // // Test to ensure all cards are highlighted after clicking 1'st card
-  // it('Should highlight all 144 cards', done => {
-  //   setTimeout(function(){ var hi = this.driver.findElement(selenium.By.css('.card')); }, 300000);
-
-  //   var element = this.driver.findElement(selenium.By.css('.card'));
-  //   element.click();
-  //   this.driver.findElements(selenium.By.className('card__highlight')).then(elements => {
-  //     expect(elements.length).toBe(144);
-  //   });
-  //   done();
-  // });
-
-  // // Test to ensure cards are un-highlighted after second click
-  // it('Should un-highlight all the cards', done => {
-  //   setTimeout(function(){ var hi = this.driver.findElement(selenium.By.css('.card')); }, 300000);
-
-  //   const element = this.driver.findElement(selenium.By.css('.card'));
-  //   element.click();
-  //   element.click();
-  //   this.driver.findElements(selenium.By.className('card__highlight')).then(elements => {
-  //     expect(elements.length).toBe(0);
-  //   });
-  //   done();
-  // });
+  // Test to ensure cards are un-highlighted after second click
+  it('Should un-highlight all the cards', done => {
+    const element = this.driver.findElement(selenium.By.css('.card'));
+    element.click();
+    element.click();
+    this.driver.findElements(selenium.By.className('card__highlight')).then(elements => {
+      expect(elements.length).toBe(0);
+    });
+    done();
+  });
 
 
 });
