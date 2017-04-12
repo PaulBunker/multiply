3cf5920ef53432b057f8477133af3383a60c84fa
Paul Bunker <paulBunker@khws.co.uk>
2017-04-12 08:55:39 +0100

    little test while waiting for docker to install

--- spec/integrationtest.js
+++ spec/integrationtest.js
@@ -2,22 +2,17 @@ require('chromedriver');
 var selenium = require('selenium-webdriver');
 
 
-
-
-
-
-
 describe('Multiply test:', function() {
 
-  var originalTimeout;
+  // var originalTimeout;
 
   // Open the local website
   beforeEach( done => {
     this.driver = new selenium.Builder()
       .forBrowser('chrome')
       .build();
-    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    // originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    // jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
     this.driver.get('http://localhost:8000/index.html').then(done);
   });
 
@@ -25,7 +20,7 @@ describe('Multiply test:', function() {
 
   // Close the website after each test is run
   afterEach( done => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    // jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
     return this.driver.quit().then(done);
 
   });
@@ -49,44 +44,44 @@ describe('Multiply test:', function() {
   });
 
 
-  // Test to ensure we highlight the
-  it('Should highlight', done => {
-    var element = this.driver.findElement(selenium.By.css('.card'));
-    element.click();
-    element.getAttribute('class').then(className => {
-      expect(className).toBe('card card__highlight');
-    });
-    done();
-  });
+  // // Test to ensure we highlight the
+  // it('Should highlight', done => {
+  //   var element = this.driver.findElement(selenium.By.css('.card'));
+  //   element.click();
+  //   element.getAttribute('class').then(className => {
+  //     expect(className).toBe('card card__highlight');
+  //   });
+  //   done();
+  // });
 
-  // Test to ensure we have the right number of cards
-  it('Should be 144 cards', done => {
-    this.driver.findElements(selenium.By.className('card')).then(elements => {
-      expect(elements.length).toBe(144);
-    });
-    done();
-  });
+  // // Test to ensure we have the right number of cards
+  // it('Should be 144 cards', done => {
+  //   this.driver.findElements(selenium.By.className('card')).then(elements => {
+  //     expect(elements.length).toBe(144);
+  //   });
+  //   done();
+  // });
 
-  // Test to ensure all cards are highlighted after clicking 1'st card
-  it('Should highlight all 144 cards', done => {
-    var element = this.driver.findElement(selenium.By.css('.card'));
-    element.click();
-    this.driver.findElements(selenium.By.className('card__highlight')).then(elements => {
-      expect(elements.length).toBe(144);
-    });
-    done();
-  });
+  // // Test to ensure all cards are highlighted after clicking 1'st card
+  // it('Should highlight all 144 cards', done => {
+  //   var element = this.driver.findElement(selenium.By.css('.card'));
+  //   element.click();
+  //   this.driver.findElements(selenium.By.className('card__highlight')).then(elements => {
+  //     expect(elements.length).toBe(144);
+  //   });
+  //   done();
+  // });
 
-  // Test to ensure cards are un-highlighted after second click
-  it('Should un-highlight all the cards', done => {
-    const element = this.driver.findElement(selenium.By.css('.card'));
-    element.click();
-    element.click();
-    this.driver.findElements(selenium.By.className('card__highlight')).then(elements => {
-      expect(elements.length).toBe(0);
-    });
-    done();
-  });
+  // // Test to ensure cards are un-highlighted after second click
+  // it('Should un-highlight all the cards', done => {
+  //   const element = this.driver.findElement(selenium.By.css('.card'));
+  //   element.click();
+  //   element.click();
+  //   this.driver.findElements(selenium.By.className('card__highlight')).then(elements => {
+  //     expect(elements.length).toBe(0);
+  //   });
+  //   done();
+  // });
 
 
 });
